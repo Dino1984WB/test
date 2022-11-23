f1a9adfd8111b213bffb45df7a7f3212d9c9e115
Williamkbukowski <>
2022-11-23 15:26:10 -0500

    structural changes to apiMain, working on home.js more

--- apiMain.py
+++ apiMain.py
@@ -1,18 +1,29 @@
 #william bukowski
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Api, Resource
 from flask import render_template
 
 #init flask application
 app=Flask(__name__, static_folder='static/', static_url_path='')
 
+
 @app.route('/', methods=['GET', 'POST'])
-def renderhome(): #function to search db for name entered
+def renderhome(): #function to search db for name entered   
     return render_template("home.html")
 
 @app.route('/', methods=['GET', 'POST'])
-def parseJson():
-    print("json parser goes here")
+def parseJson(nameS):
+    return jsonify({"name":nameS})
+
+@app.route('/', methods=['GET', 'POST'])
+class homePage(): 
+    
+    __homePage__ = "homePage"
+
+    def test(test):
+        return test
+    def test2(test2):
+        return test2
 
 
 if __name__ == "__main__": 
--- static/home.js
+++ static/home.js
@@ -1,24 +1,26 @@
 //home page js
 
-//grab name from input
-let name = document.getElementById("name")
+//grab name from input in home.html
+let nameS = document.getElementById('name');
+let nameString = JSON.stringify(nameS);
+console.log(nameS);
 
+//function triggered by html button
 async function fetchName () {
   
-    console.log("button pressed for search") //test
+  let response = await fetch('http://127.0.0.1:8000', {
+    method: 'POST',
+    body: JSON.stringify(nameString)
+  });
 
-    let response = await fetch('http://127.0.0.1:8000', {
-      method: 'POST',
-      body: JSON.stringify(name)
-    });
 
+  //response is good or bad, ie connection good or bad  
+  if (response.ok) {
+    let json = response.json();
+    console.log("Good Response:200" + json); 
+  } else {
+    alert("Reponse not ok" + response.status);
+    console.log(response.status);
+  }
     
-    if (response.ok) {
-      let json = response.json();
-      console.log("Response:200"); 
-    } else {
-      alert("Reponse not ok" + response.status);
-      console.log(response.status);
-    }
-     
 }
